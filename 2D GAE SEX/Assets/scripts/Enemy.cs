a0bd2b793334a57395c10a3ac3a99480b0bc74e3
B-MAXIMUM <bgmundy5223@gmail.com>
2022-10-25 10:59:32 -0700

    0.3

---
+++ 2D GAE SEX/Assets/scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed = 1;
+    private Rigidbody2D _enemyRb;
+    private GameObject _Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyRb = GetComponent<Rigidbody2D>();
+        _Player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lookDirection = (_Player.transform.position - transform.position).normalized;
+        _enemyRb.AddForce(lookDirection * Speed);
+    }
+}
